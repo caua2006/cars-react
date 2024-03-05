47a7581400d7b6edc0caada36e94b0d7bd8100fb
caua2006 <caua2006carvalho@gmail.com>
2024-03-05 09:46:17 -0300

    Changes into App and CarSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { CarSection } from "./components/CarSection";
 
 export  function App() {
+
+  const cars = [{
+    id: 1,
+    name: "Sedans",
+    description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    color: "Orange"
+  },
+  {
+    id: 2,
+    name: "Suvs",
+    description:"Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    color: "Blue"
+  },
+  {
+    id: 3,
+    name: "Luxury",
+    description:"Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+  }
+  ]
   return (
     <div className="flex justify-center items-center h-[100vh]">
-      <CarSection />
-      <CarSection />
-      <CarSection />
+      {cars.map(car => <CarSection key={car.id} typeCar={car}/>)}
     </div>
   )
 }
--- src/components/CarSection.jsx
+++ src/components/CarSection.jsx
@@ -1,11 +1,17 @@
 import LogoSedans from '../assets/icon-sedans.svg'
-export function CarSection() {
+import LogoSuvs from '../assets/icon-suvs.svg'
+import LogoLuxury from '../assets/icon-luxury.svg'
+
+
+export function CarSection(props) {
+  console.log(props.typeCar);
+
+  
   return (
     <section className='bg-orange-500 p-12 max-w-xs flex flex-col justify-between gap-5 rounded-s-md'>
-      <img className='w-16 h-10' src={LogoSedans} alt="" />
-      <h2 className='text-2xl text-white'>Sedans</h2>
-      <p className='text-slate-200'>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
-      or on your next road trip.</p>
+      <img className='w-16 h-10'  alt="" />
+      <h2 className='text-2xl text-white'>{props.typeCar.name}</h2>
+      <p className='text-slate-200'>{props.typeCar.description}</p>
       <button className='bg-white rounded-full p-2 text-orange-500'>Learn more</button>
   </section>
   )
