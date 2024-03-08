5f5c6d59eb4ee534e30f5348c5d8e517c1f3bcc1
EA - Cauã Carvalho De Oliveira <caua_carvalho01@educadventista.org>
2024-03-08 13:55:33 -0300

    Commit with colors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import { CarSection } from "./components/CarSection";
+import LogoSedans from './assets/icon-sedans.svg'
+import LogoSuvs from './assets/icon-suvs.svg'
+import LogoLuxury from './assets/icon-luxury.svg'
 
 export  function App() {
 
   const cars = [{
     id: 1,
+    img: LogoSedans,
     name: "Sedans",
     description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
-    color: "Orange"
+    color: "O"
   },
   {
     id: 2,
+    img: LogoSuvs,
     name: "Suvs",
     description:"Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
-    color: "Blue"
+    color: "B"
   },
   {
     id: 3,
+    img: LogoLuxury,
     name: "Luxury",
     description:"Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    color: "G"
+    
   }
   ]
   return (
--- src/components/CarSection.jsx
+++ src/components/CarSection.jsx
@@ -1,19 +1,29 @@
-import LogoSedans from '../assets/icon-sedans.svg'
-import LogoSuvs from '../assets/icon-suvs.svg'
-import LogoLuxury from '../assets/icon-luxury.svg'
+// import LogoSedans from '../assets/icon-sedans.svg'
+// import LogoSuvs from '../assets/icon-suvs.svg'
+// import LogoLuxury from '../assets/icon-luxury.svg'
 
 
-export function CarSection(props) {
-  console.log(props.typeCar);
+export function CarSection({ typeCar }) {
+  let colorClassName = ""
+  if (typeCar.color == "O"){
+    colorClassName = 'bg-orange-500 p-12 max-w-xs flex flex-col justify-between gap-5 rounded-s-md'
+  }
+  if (typeCar.color == "B"){
+    colorClassName = 'bg-teal-700 p-12 max-w-xs flex flex-col justify-between gap-5 rounded-s-md'
+  }
+  if (typeCar.color == "G"){
+    colorClassName = 'bg-cyan-700 p-12 max-w-xs flex flex-col justify-between gap-5 rounded-s-md'
+  }
 
-  
+
+  console.log(colorClassName);
   return (
-    <section className='bg-orange-500 p-12 max-w-xs flex flex-col justify-between gap-5 rounded-s-md'>
-      <img className='w-16 h-10'  alt="" />
-      <h2 className='text-2xl text-white'>{props.typeCar.name}</h2>
-      <p className='text-slate-200'>{props.typeCar.description}</p>
-      <button className='bg-white rounded-full p-2 text-orange-500'>Learn more</button>
-  </section>
+    <section className={colorClassName}>
+      <img className='w-16 h-10' src={typeCar.img}   alt="" />
+      <h2 className='text-2xl text-white'>{typeCar.name}</h2>
+      <p className='text-slate-200'>{typeCar.description}</p>
+      <button className='bg-white rounded-full p-2 text-orange-500 hover:bg-transparent hover:text-white hover:border'>Learn more</button>
+    </section>
   )
 }
 
